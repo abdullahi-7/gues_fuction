00b3edcdc3621ecfb51bef4d443ca8588ac5d42a
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-18 09:07:36 +0300

    channge the variable

--- gues_f.py
+++ gues_f.py
@@ -40,3 +40,12 @@ def gues():
     else:
         print("your number is small", var )
 
+    v = int(input("gues a number? "))
+    var = random.randint(1,4) 
+    if v != var:
+        print("wrong")
+    elif v > var:
+        print("your number is bigger", var)
+    else:
+        print("your number is small", var )
+
